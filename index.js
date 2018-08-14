bec42afc298e9efe248af915e4fcef31fa4574aa
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-14 21:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var dodger = document.getElementById('dodger');
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
+  var rightNumbers = dodger.style.right.replace('px', '');
   var right = parseInt(rightNumbers, 10);
   if (right > 0) {
     dodger.style.right = `${right - 1}px`;
@@ -12,5 +12,5 @@ document.addEventListener('keydown', function(e) {
   if (e.which === 39) {
     moveDodgerRight();
   }
-})
+});
     
